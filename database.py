3c437ccc8b0fe4b045187ca70a52368c47df722c
tanushmal <63530903+tanushmal@users.noreply.github.com>
2020-05-25 16:47:54 +0530

    path updated

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from flask import g
 import sqlite3
 
 def connect_db():
-    sql = sqlite3.connect('sqlite3/customerAPI.db')
+    sql = sqlite3.connect('/home/tanushr/customerAPI/sqlite3/customerAPI.db')
     sql.row_factory = sqlite3.Row
     return sql
 
